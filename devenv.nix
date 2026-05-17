fddf0c65490f9dfa41405ea52d922f185e95ac8b
Tibo Verk <tibo@pookie.noho.st>
2026-05-17 21:32:49 +0200

    add linux requirements to devenv

--- devenv.nix
+++ devenv.nix
@@ -8,15 +8,22 @@
   # https://devenv.sh/languages/
   languages.rust = {
     enable = true;
+    rustflags = "-C target-cpu=native";
   };
-
   
   packages = [
     pkgs.git
     pkgs.lazygit
     pkgs.pkg-config
     pkgs.vips
+    pkgs.openssl
+    pkgs.llvmPackages.libclang
+    pkgs.llvmPackages.libcxxClang
   ];
 
+    env = {
+    # Point bindgen to the correct libclang directory inside the Nix store
+    LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+    };
   # See full reference at https://devenv.sh/reference/options/
 }
